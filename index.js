f101fa6e0331dde61ff769a47743755df034d1cc
iop0175 <iop0175@naver.com>
2025-08-18 21:15:04 +0900

    commit

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-import express from 'express'
-import cors from 'cors'
-const app = express()
-app.use(cors())
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
 app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+    res.sendFile(__dirname + '/index.html');
+});
+
 app.get('/sound/:name', (req, res) => {
-    const { name } = req.params
-    if(name == "dog"){
-        res.json({"sound":"멍멍"})
-    }else if(name == "cat"){
-        res.json({"sound":"야옹"})
-    }else{
-        res.json({"sound":"알수없음"})
+    const { name } = req.params;
+    if (name == "dog") {
+        res.json({ "sound": "멍멍" });
+    } else if (name == "cat") {
+        res.json({ "sound": "야옹" });
+    } else {
+        res.json({ "sound": "알수없음" });
     }
-})
-app.listen(3000)
+});
+
+app.listen(3000);
